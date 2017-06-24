98d49e49e503a80ef393603c7d75de15d30440b5
syakesaba <syakesaba@gmail.com>
2017-06-24 15:56:04 +0900

    Revert "fixed local-ref"
    This reverts commit ebce8adac751379c686b7a2c083ad1dd5a31703e.
    
    Conflicts:
    	services/xss/srv_xss.cpp
    	services/xss/srv_xss.h

---
+++ services/xss/srv_xss.h
@@ -0,0 +1,27 @@
+/*
+    Anti-XSS c-icap service
+    srv_xss.h
+    Copyright (C) 2013  SYA-KE
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SRV_XSS_H_
+#define SRV_XSS_H_
+
+#include "c_icap/c-icap.h"
+
+CI_DECLARE_MOD_DATA ci_service_module_t service;
+
+#endif /* SRV_XSS_H_ */
